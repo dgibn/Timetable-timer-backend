6029d11d23fcedef923ba0d50eb0f48236b230bf
dataraj369 <125258437+dataraj369@users.noreply.github.com>
2023-02-25 18:45:15 +0530

    Added Events
    Created a class Events in models.py

--- backend_repo/timetable_back/main/urls.py
+++ backend_repo/timetable_back/main/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns= [
     path("day/",views.day,name="day"),
     path("week/",views.week,name="week"),
-    path("month/",views.month,name="month")
+    path("month/",views.month,name="month"),
 ]
--- backend_repo/timetable_back/main/views.py
+++ backend_repo/timetable_back/main/views.py
@@ -3,11 +3,13 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-def day(resonse):
+def day(response):
     return HttpResponse("<h1>Calender day view</h1>")
 
-def week(resonse):
+def week(response):
     return HttpResponse("<h1>Calender week view</h1>")
 
-def month(resonse):
+def month(response):
     return HttpResponse("<h1>Calender month view</h1>")
+
+
