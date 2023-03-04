2ea38df15acae3a1de44c5295a03e76a5e746333
UtsavJha123 <125276792+UtsavJha123@users.noreply.github.com>
2023-03-04 20:36:37 +0530

    Update serializers.py

--- timetable_back/main/serializers.py
+++ timetable_back/main/serializers.py
@@ -1,6 +1,12 @@
 from rest_framework import serializers
+from main.models import Event
 
 class timetable_serializer(serializers.Serializer):
+    class Meta:
+        model = Event
+        fields = ('id', 'name', 'desc', 'stime', 'etime', 'done')
+        read_only_fields = ('id',)
+
     name = serializers.CharField(max_length=200)
     desc = serializers.CharField(max_length=500, default="Description")
     stime = serializers.DateTimeField
