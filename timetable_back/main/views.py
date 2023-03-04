1824a3dc137ecc93ed28dd22ebe94aca948d6b97
UtsavJha123 <125276792+UtsavJha123@users.noreply.github.com>
2023-03-04 16:19:08 +0530

    updated views.py

--- timetable_back/main/views.py
+++ timetable_back/main/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render,redirect
 from main.models import Event
 from main.forms import Eventreg
+from .serializers import timetable_serializer
+from rest_framework.renderers import JSONRenderer
+from django.http import HttpResponse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+
+ 
+
+
 
 
 # Create your views here.
@@ -21,12 +31,14 @@ def add(request):
         form = Eventreg(request.POST or None)
         if form.is_valid():
             form.save()
-        return redirect('day')
+        print("data saved")
+        return redirect('add')
 
     else:
         return render(request, 'add.html', {})
     
 def update(request, event_id):
+    
     update_event = Event.objects.get(pk=event_id)
     form = Eventreg(request.POST or None, instance=update_event)
     if form.is_valid():
@@ -40,4 +52,26 @@ def delete(request, event_id):
     delete_event = Event.objects.get(pk=event_id)
     delete_event.delete()
     return redirect('day')
+
+@api_view(['GET'])
+def Event_detail(request,pk):
+    event = Event.objects.get(id=pk)
+    serializer = timetable_serializer(event)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def Event_detail_all(request):
+        event = Event.objects.all()
+        serializer = timetable_serializer(event,many=True)
+        return Response(serializer.data)
+
+
+
+
+
+
+
     
+
+    
+
