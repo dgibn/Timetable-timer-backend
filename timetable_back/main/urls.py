4fe7861a83d3274c77d75babcf3fbcf84d355b00
UtsavJha123 <125276792+UtsavJha123@users.noreply.github.com>
2023-03-07 22:43:46 +0530

    Update urls.py

--- timetable_back/main/urls.py
+++ timetable_back/main/urls.py
@@ -3,18 +3,11 @@ from . import views
 from main import views
 
 urlpatterns= [
-    path("day/",views.day,name="day"),
-    path("week/",views.week,name="week"),
-    path("month/",views.month,name="month"),
-    path("add",views.add, name="add"),
-    path("update/<event_id>/",views.update, name="update"),
-    path("delete/<event_id>/",views.delete, name="delete"),
+    
+    path("add_event",views.add_Event, name="add"),
+    path("update_event/<int:pk>",views.update_event, name="update"),
+    path("event_info/<int:pk>/delete/", views.delete_event, name='delete'),
     path("event_info/<int:pk>",views.Event_detail, name="event_detail") ,
-    
     path("event_info/",views.Event_detail_all, name="event_detail_all"),
-    
-    
-
-    
-    
+        
 ]
