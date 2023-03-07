e489e4bcb23a71ae85a128cc6490a75cbffbd128
UtsavJha123 <125276792+UtsavJha123@users.noreply.github.com>
2023-03-07 22:42:31 +0530

    Update serializers.py

--- timetable_back/main/serializers.py
+++ timetable_back/main/serializers.py
@@ -9,7 +9,20 @@ class timetable_serializer(serializers.Serializer):
 
     name = serializers.CharField(max_length=200)
     desc = serializers.CharField(max_length=500, default="Description")
-    stime = serializers.DateTimeField
-    etime = serializers.DateTimeField
-    done = serializers.BooleanField
+    stime = serializers.DateTimeField()
+    etime = serializers.DateTimeField()
+    done = serializers.BooleanField()
+
+
+    def create(self, validated_data):
+        return Event.objects.create(**validated_data)
+    
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get('name', instance.name)
+        instance.desc = validated_data.get('desc', instance.desc)
+        instance.stime = validated_data.get('stime', instance.stime)
+        instance.etime = validated_data.get('etime', instance.etime)
+        instance.done = validated_data.get('done', instance.done)
+        instance.save()
+        return instance
 
