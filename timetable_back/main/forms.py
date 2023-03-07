8a4a3def6712e3ad70945df96240f65f52ee4612
UtsavJha123 <125276792+UtsavJha123@users.noreply.github.com>
2023-03-07 22:42:07 +0530

    Delete forms.py

--- timetable_back/main/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from main.models import Event
-
-class Eventreg(forms.ModelForm):
-    class Meta:
-        model = Event
-        fields = ['name', 'desc', 'stime', 'etime', 'done']
